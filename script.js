b45dbdd345b91cdd63699fae8473419fa038839a
Vynzter <vynraven@gmail.com>
2025-01-16 16:39:34 +0700

    update JS for change user greeting name

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
-//Ini File Javascript
+//This Javascript File is used for the RevoU SEFC Task Portfolio Website - Kevin Ho
 
-//Our Profile
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs((slideIndex += n));
-}
-
-function showDivs(n) {
-    var i;
-    var imgList = document.getElementsByClassName("img-slideshow");
-
-    if (n > imgList.length)  slideIndex = 1; 
-    else if (n < 1) slideIndex = imgList.length; 
-
-    for (i = 0; i < imgList.length; i++) {
-        imgList[i].style.display = "none"; // Sembunyikan semua gambar
-    }
-    imgList[slideIndex - 1].style.display = "block"; // Tampilkan gambar yang sesuai
-}
-
-// Auto-slide setiap 3 detik
-setInterval(() => {
-    plusDivs(1);
-}, 3000);
-
-///////
 //Our Profile
 let indexBanner = 0;
 
@@ -60,61 +33,71 @@ setInterval(nextBanner, 3000); //Auto-slide every 3 seconds
 
 console.log('Hello World!');
 
-// Function to validate form
+// Function to validate form data
 function formValidation() {
     let nameInput = document.getElementById('name-input').value;
-    console.log(nameInput);
+    let birthdateInput = document.getElementById('birthdate-input').value;
+    let genderInput = document.querySelector('input[name="gender"]:checked');
+    let messageInput = document.getElementById('message-input').value;
+
     // Condition for validation form
-    if (nameInput ==""){
-        alert('name must be filled out!');
-    } else {
-        // Display result form
-        document.getElementById('result-form').innerHTML = nameInput;
+    if (nameInput ===""|| birthdateInput ==="" || !genderInput || messageInput ==="") {
+        alert('All field must be filled out!');
+        return;
+    } 
+    // Format waktu sesuai contoh (Fri Jun 17 2022 11:27:28 GMT+0700)
+    let currentTime = new Date().toString();
+
+        // Display result in result-form box
+        document.getElementById('result-form').style.display = 'block';
+        document.getElementById ('result-form').innerHTML = `
+            <strong>Current Time:</strong> ${currentTime} <br><br>
+            <strong>Name:</strong> ${nameInput} <br>
+            <strong>Birthdate:</strong> ${birthdateInput} <br>
+            <strong>Gender:</strong> ${genderInput.value} <br>
+            <strong>Message:</strong> ${messageInput}
+        `;
+
+        // Update greeting name on homepage
+        let greetingElement = document.getElementById("user-greeting");
+        if (greetingElement) {
+        greetingElement.textContent = nameInput;
     }
+        // Reset form setelah submit
+        document.getElementById("messageForm").reset();
+    } 
+    // add event listener to form
+    document.getElementById('submit-btn').addEventListener('click', formValidation);
 
-    
-}document.getElementById('result-form').innerHTML = birthdateInput;
-
-document.getElementById('submit-btn').addEventListener('click',formValidation);
-
-//////////
-
-
-
-
+// Prevent reload page after submit messageForm    
 document.getElementById("messageForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Mencegah reload halaman
+    event.preventDefault(); 
 
-// Mengambil data dari form
-let name = document.getElementById("name").value;
-let birthdate = document.getElementById("birthdate").value;
-let gender = document.querySelector('input[name="gender"]:checked');
-let message = document.getElementById("message").value;
-
-if (!gender) {
-    alert("Pilih jenis kelamin!");
-    return;
-}
-
-// Format waktu sesuai contoh (Fri Jun 17 2022 11:27:28 GMT+0700)
-let currentTime = new Date().toString();
-
-// Menampilkan hasil di box output
-let outputBox = document.getElementById("outputBox").style.display = "block";
-outputBox.innerHTML = `
-    <strong>Current time:</strong> ${currentTime} <br>
-    <strong>Nama:</strong> ${name} <br>
-    <strong>Tanggal Lahir:</strong> ${birthdate} <br>
-    <strong>Jenis Kelamin:</strong> ${gender.value} <br>
-    <strong>Pesan:</strong> ${message}
-`;
-
-//mengubah nama di homepage
-let greetingElement = document.getElementById("user-greeting");
-if (greetingElement) {
-    greetingElement.textContent = name;
-}
-
- // Reset form setelah submit
- document.getElementById("messageForm").reset();
 });
+
+//////////////////////////
+
+//Our Profile
+
+/*var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+    var i;
+    var imgList = document.getElementsByClassName("img-slideshow");
+
+    if (n > imgList.length)  slideIndex = 1; 
+    else if (n < 1) slideIndex = imgList.length; 
+
+    for (i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = "none"; // Sembunyikan semua gambar
+    }
+    imgList[slideIndex - 1].style.display = "block"; // Tampilkan gambar yang sesuai
+}
+
+// Auto-slide setiap 3 detik
+//setInterval(() => {plusDivs(1);}, 3000);*/
